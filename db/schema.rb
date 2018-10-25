e83a44cc65a52c55f96137ab6fa64d92a5b05050
DanielaRF06 <dani_ruizdgo@hotmail.com>
2018-10-25 00:55:34 +0000

    feat: CRUD clientes y produtos, conexion heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_003814) do
+ActiveRecord::Schema.define(version: 2018_10_25_005126) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "rfc"
     t.string "nom_cliente"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.string "nombre_producto"
+    t.float "precio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
